e83500655d98ef8db5b0811a88e42944ed53e107
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-10-08 21:37:39 +0300

    [LC-01] - Method to find out whether a number is a polyndrome or not

---
+++ src/Polindrom.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Polindrom {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int x = sc.nextInt();
+
+        if (!isPalindrome(x)) {
+            System.out.println("Not a palindrome.");
+        } else {
+            System.out.println("Palindrome.");
+        }
+
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int firstXNumber = x; // I store the value x.
+        int reverseNumber = 0;
+
+        while (x > 0) {
+
+            int lastDigit = x % 10;
+            reverseNumber = reverseNumber * 10 + lastDigit;
+            x = x / 10;
+
+        }
+
+        return firstXNumber == reverseNumber;
+
+    }
+
+}
